70364f79f87375a242ca4ba405fe275d9b19cb74
Sergey Shadrin <36serg6@gmail.com>
2020-09-11 14:07:42 +0300

    Changes for new repo - rivne

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,9 +10,9 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, '134.209.237.64'
-set :deploy_to, "/var/www/r4yzp/"
-set :repository, "git@github.com:36serg6/publicwhip-zaporizhia.git"
+set :domain, '188.166.28.73'
+set :deploy_to, "/var/www/r4yrivne/"
+set :repository, "git@github.com:36serg6/publicwhip-rivne.git"
 set :branch, 'policy-production'
 
 # For system-wide RVM install.
